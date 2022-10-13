58861e6b3b9b4b358ea899823172fb58dcb83161
Manu Sharma <ms5663830@gmail.com>
2022-10-13 17:33:04 +0530

    constructor and setter injection in annotation

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,41 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-
+	
+	private FortuneService fortuneService;
+	
+	//define a default constructor
+	public TennisCoach() {
+		System.out.println("TennisCoach:  inside default constructor");
+	}
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+		
+	}*/
+	// define a setter method
+	
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println("TennisCoach : inside setFortuneService() method");
+		fortuneService = theFortuneService ;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
 		return "Practice your backhand Volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 
 }
